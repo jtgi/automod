7e193e520b501b60e8f4015fc165a35b28f43caf
jtgi <j@jtgi.me>
2024-01-30 22:00:15 +0900

    add video

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -19,31 +19,40 @@ export default function Login() {
   return (
     <div className="px-8 h-full w-full flex flex-col items-center justify-center min-h-screen">
       <div className="max-w-xl">
-        <h1 className="text-6xl logo">glass</h1>
-        <h2 className="font-normal mb-8">Gate content with Farcaster Frames</h2>
+        <h1 className="text-center text-5xl logo">glass</h1>
+        <h2 className="text-center text-lg font-normal mb-4">
+          Gate content with Farcaster Frames
+        </h2>
+
+        <video
+          className="w-full my-4 rounded-lg shadow-md"
+          autoPlay
+          loop
+          muted
+          controls
+          playsInline
+          src="/demo-vid.mp4"
+        />
+
+        {/* play demo-vid.mp4 video here with autoplay */}
 
         <div className="space-y-4">
           <ul className="ml-4 list-outside list-disc space-y-2">
             <li>
               Create exclusive content for followers, ERC-20 and ERC-721 token
-              holders. Require minimum token balances or even specific tokens.
+              holders.
             </li>
             <li>
               Boost engagement by requiring a post be liked, recasted, followed
               before visible.
             </li>
             <li>
-              Reveal any text content, image, even other frames with more
-              redemption formats coming soon.
+              Reveal any text content, image, even other frames. (more soon)
             </li>
             <li>Zero code required.</li>
           </ul>
 
-          <p className="text-xs text-gray-600 ml-4">
-            Available on Mainnet, Base, Optimism, and Zora
-          </p>
-
-          <div className="pt-6">
+          <div className="pt-2 flex flex-col justify-center">
             <Button asChild className="w-full sm:w-auto">
               <Link
                 className="no-underline"
@@ -51,9 +60,12 @@ export default function Login() {
                 target="_blank"
                 rel="noreferrer"
               >
-                Preorder <ArrowTopRightIcon className="ml-1" />
+                Preorder Now <ArrowTopRightIcon className="ml-1" />
               </Link>
             </Button>
+            <span className="text-xs text-gray-500 block text-center mt-1">
+              Limited quantity, ships early Feb
+            </span>
           </div>
         </div>
       </div>
