a4f16eeec7c150423b7249a169d8b3638446830c
jtgi <j@jtgi.me>
2024-02-02 11:51:25 +0900

    add cache

--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -5,9 +5,10 @@ import invariant from "tiny-invariant";
 import { db } from "~/lib/db.server";
 import { generateFrame, getSharedEnv } from "~/lib/utils.server";
 import { pageFollowersDeep } from "~/lib/neynar.server";
-import { chainByChainId, clientsByChainId } from "~/lib/viem.server";
+import { clientsByChainId } from "~/lib/viem.server";
 import axios from "axios";
 import { MessageResponse } from "~/lib/types";
+import { cache } from "~/lib/cache.server";
 
 type FrameResponseArgs = {
   title?: string;
@@ -79,174 +80,183 @@ export async function action({ request, params }: LoaderFunctionArgs) {
     return json({ error: "Invalid url" }, { status: 400 });
   }
 
-  if (frame.requireSomeoneIFollow) {
-    if (message.action.interactor.fid !== message.action.cast.author.fid) {
-      const followers = await pageFollowersDeep({
-        fid: message.action.interactor.fid,
-      });
+  const cacheKey = `frameResult:${message.action.cast.hash}:${message.action.interactor.fid}:${frame.id}`;
+  const cached = cache.get<string>(cacheKey);
 
-      const isValid = followers.some(
-        (f) => f.fid == message.action.cast.author.fid
-      );
-
-      if (!isValid) {
-        return frameResponse({
-          image: await generateFrame(frame, `Restricted to followers only`),
-          buttons: [{ text: "Try Again" }],
+  if (cached === "true") {
+    if (frame.requireSomeoneIFollow) {
+      if (message.action.interactor.fid !== message.action.cast.author.fid) {
+        const followers = await pageFollowersDeep({
+          fid: message.action.interactor.fid,
         });
+
+        const isValid = followers.some(
+          (f) => f.fid == message.action.cast.author.fid
+        );
+
+        if (!isValid) {
+          return frameResponse({
+            image: await generateFrame(frame, `Restricted to followers only`),
+            buttons: [{ text: "Try Again" }],
+          });
+        }
       }
     }
-  }
 
-  if (frame.requireFollow) {
-    if (message.action.interactor.fid !== message.action.cast.author.fid) {
-      const followers = await pageFollowersDeep({
-        fid: message.action.cast.author.fid,
-      });
-
-      const isValid = followers.some(
-        (f) => f.fid == message.action.interactor.fid
-      );
-
-      if (!isValid) {
-        return frameResponse({
-          image: await generateFrame(frame, "Must follow to reveal"),
-          buttons: [{ text: "Try Again" }],
+    if (frame.requireFollow) {
+      if (message.action.interactor.fid !== message.action.cast.author.fid) {
+        const followers = await pageFollowersDeep({
+          fid: message.action.cast.author.fid,
         });
+
+        const isValid = followers.some(
+          (f) => f.fid == message.action.interactor.fid
+        );
+
+        if (!isValid) {
+          return frameResponse({
+            image: await generateFrame(frame, "Must follow to reveal"),
+            buttons: [{ text: "Try Again" }],
+          });
+        }
       }
     }
-  }
 
-  if (frame.requireHoldERC20) {
-    const user = message.action.interactor;
+    if (frame.requireHoldERC20) {
+      const user = message.action.interactor;
 
-    if (!user.verifications.length) {
-      return frameResponse({
-        image: await generateFrame(
-          frame,
-          "Must link an address to your Farcaster account"
-        ),
-        buttons: [{ text: "Try Again" }],
-      });
-    }
-
-    invariant(frame.requireERC20NetworkId, "Missing network id");
-    invariant(frame.requireERC20ContractAddress, "Missing network id");
-
-    const minBalanceRequired = frame.requireERC20MinBalance || "0";
-    const client = clientsByChainId[frame.requireERC20NetworkId];
-    const contract = getContract({
-      address: getAddress(frame.requireERC20ContractAddress),
-      abi: erc20Abi,
-      client,
-    });
-
-    const balances = await Promise.all(
-      user.verifications.map((add) =>
-        contract.read.balanceOf([getAddress(add)])
-      )
-    );
-    const decimals = await contract.read.decimals();
-    const minBalanceBigInt = parseUnits(minBalanceRequired, decimals);
-    const sum = balances.reduce((a, b) => a + b, BigInt(0));
-    const isValid = sum >= minBalanceBigInt;
-
-    if (!isValid) {
-      const info = await contract.read.symbol();
-      return frameResponse({
-        image: await generateFrame(
-          frame,
-          frame.requireERC20MinBalance == "0"
-            ? "Must hold a balance to reveal"
-            : `Must hold at least ${frame.requireERC20MinBalance} $${info} to reveal`
-        ),
-        buttons: [{ text: "Try Again" }],
-      });
-    }
-  }
-
-  if (frame.requireHoldERC721) {
-    const user = message.action.interactor;
-    if (!user.verifications.length) {
-      return frameResponse({
-        image: await generateFrame(
-          frame,
-          "Must link an address to your Farcaster account"
-        ),
-        buttons: [{ text: "Try Again" }],
-      });
-    }
-
-    invariant(frame.requireERC721NetworkId, "Missing network id");
-    invariant(frame.requireERC721ContractAddress, "Missing contract address");
-
-    const client = clientsByChainId[frame.requireERC721NetworkId];
-    const contract = getContract({
-      address: getAddress(frame.requireERC721ContractAddress),
-      abi: erc721Abi,
-      client,
-    });
-
-    if (frame.requireERC721TokenId !== null) {
-      const owner = await contract.read.ownerOf([
-        BigInt(frame.requireERC721TokenId),
-      ]);
-      const isValid = user.verifications.some(
-        (address) => address.toLowerCase() === owner.toLowerCase()
-      );
-
-      if (!isValid) {
-        const name = await contract.read.name();
-
+      if (!user.verifications.length) {
         return frameResponse({
           image: await generateFrame(
             frame,
-            `${name} #${frame.requireERC721TokenId} is required`
+            "Must link an address to your Farcaster account"
           ),
           buttons: [{ text: "Try Again" }],
         });
-        // TODO: add a nice feature to show the user the token they need to hold
       }
-    } else {
+
+      invariant(frame.requireERC20NetworkId, "Missing network id");
+      invariant(frame.requireERC20ContractAddress, "Missing network id");
+
+      const minBalanceRequired = frame.requireERC20MinBalance || "0";
+      const client = clientsByChainId[frame.requireERC20NetworkId];
+      const contract = getContract({
+        address: getAddress(frame.requireERC20ContractAddress),
+        abi: erc20Abi,
+        client,
+      });
+
       const balances = await Promise.all(
-        user.verifications.map((address) =>
-          contract.read.balanceOf([getAddress(address)])
+        user.verifications.map((add) =>
+          contract.read.balanceOf([getAddress(add)])
         )
       );
-      const isValid = balances.some((balance) => balance > BigInt(0));
+      const decimals = await contract.read.decimals();
+      const minBalanceBigInt = parseUnits(minBalanceRequired, decimals);
+      const sum = balances.reduce((a, b) => a + b, BigInt(0));
+      const isValid = sum >= minBalanceBigInt;
 
       if (!isValid) {
-        const name = await contract.read.name();
+        const info = await contract.read.symbol();
         return frameResponse({
-          image: await generateFrame(frame, `${name} holders only`),
-          buttons: [{ text: "Try Again" }],
-        });
-      }
-    }
-  }
-
-  if (frame.requireLike || frame.requireRecast) {
-    if (frame.requireRecast) {
-      const isValid = message.action.cast.viewer_context.recasted;
-
-      if (!isValid) {
-        return frameResponse({
-          image: await generateFrame(frame, "Must recast to reveal"),
+          image: await generateFrame(
+            frame,
+            frame.requireERC20MinBalance == "0"
+              ? "Must hold a balance to reveal"
+              : `Must hold at least ${frame.requireERC20MinBalance} $${info} to reveal`
+          ),
           buttons: [{ text: "Try Again" }],
         });
       }
     }
 
-    if (frame.requireLike) {
-      const isValid = message.action.cast.viewer_context.liked;
-
-      if (!isValid) {
+    if (frame.requireHoldERC721) {
+      const user = message.action.interactor;
+      if (!user.verifications.length) {
         return frameResponse({
-          image: await generateFrame(frame, "Must like to reveal"),
+          image: await generateFrame(
+            frame,
+            "Must link an address to your Farcaster account"
+          ),
           buttons: [{ text: "Try Again" }],
         });
       }
+
+      invariant(frame.requireERC721NetworkId, "Missing network id");
+      invariant(frame.requireERC721ContractAddress, "Missing contract address");
+
+      const client = clientsByChainId[frame.requireERC721NetworkId];
+      const contract = getContract({
+        address: getAddress(frame.requireERC721ContractAddress),
+        abi: erc721Abi,
+        client,
+      });
+
+      if (frame.requireERC721TokenId !== null) {
+        const owner = await contract.read.ownerOf([
+          BigInt(frame.requireERC721TokenId),
+        ]);
+        const isValid = user.verifications.some(
+          (address) => address.toLowerCase() === owner.toLowerCase()
+        );
+
+        if (!isValid) {
+          const name = await contract.read.name();
+
+          return frameResponse({
+            image: await generateFrame(
+              frame,
+              `${name} #${frame.requireERC721TokenId} is required`
+            ),
+            buttons: [{ text: "Try Again" }],
+          });
+          // TODO: add a nice feature to show the user the token they need to hold
+        }
+      } else {
+        const balances = await Promise.all(
+          user.verifications.map((address) =>
+            contract.read.balanceOf([getAddress(address)])
+          )
+        );
+        const isValid = balances.some((balance) => balance > BigInt(0));
+
+        if (!isValid) {
+          const name = await contract.read.name();
+          return frameResponse({
+            image: await generateFrame(frame, `${name} holders only`),
+            buttons: [{ text: "Try Again" }],
+          });
+        }
+      }
     }
+
+    if (frame.requireLike || frame.requireRecast) {
+      if (frame.requireRecast) {
+        const isValid = message.action.cast.viewer_context.recasted;
+
+        if (!isValid) {
+          return frameResponse({
+            image: await generateFrame(frame, "Must recast to reveal"),
+            buttons: [{ text: "Try Again" }],
+          });
+        }
+      }
+
+      if (frame.requireLike) {
+        const isValid = message.action.cast.viewer_context.liked;
+
+        if (!isValid) {
+          return frameResponse({
+            image: await generateFrame(frame, "Must like to reveal"),
+            buttons: [{ text: "Try Again" }],
+          });
+        }
+      }
+    }
+
+    cache.set(cacheKey, "true", 60);
+  } else {
+    console.log("serving from cache", { cacheKey, cached });
   }
 
   if (frame.revealType === "text") {
