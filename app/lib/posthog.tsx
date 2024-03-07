c4c388435d1a3775118247c0e46ee7080cfc5648
jtgi <j@jtgi.me>
2024-03-07 14:27:53 +0900

    ship it

--- app/lib/posthog.tsx
+++ app/lib/posthog.tsx
@@ -1,20 +1,29 @@
 import { useLocation } from "@remix-run/react";
 import { useEffect } from "react";
-import { phClient } from "./posthog.client";
 import { User } from "@prisma/client";
+import { posthog } from "posthog-js";
 
 export function usePosthog(props: { user: User | null }) {
   const location = useLocation();
+  const [clientSetup, setClientSetup] = useState(false);
 
   useEffect(() => {
+    if (!clientSetup) {
+      posthog.init("phc_xSfqRtKRSoH5A9kPcNkI7J2HNakfQ5KRhFiwVujq8WL", {
+        api_host: "https://app.posthog.com",
+      });
+
+      setClientSetup(true);
+    }
+
     if (props.user) {
-      phClient?.identify(props.user.id, {
+      posthog.identify(props.user.id, {
         name: props.user.name,
       });
     }
   }, [props.user]);
 
   useEffect(() => {
-    phClient?.capture("$pageview");
+    posthog.capture("$pageview");
   }, [location]);
 }
