a841a504bfa20f82ad2eb7970550481b241f0552
jtgi <j@jtgi.me>
2024-05-09 14:23:02 +0900

    rotate thru 429s

--- app/lib/viem.server.ts
+++ app/lib/viem.server.ts
@@ -1,47 +1,42 @@
-import { createPublicClient, http } from "viem";
+import { createPublicClient, fallback, http } from "viem";
 import { arbitrum, base, mainnet, zora, optimism } from "viem/chains";
 
 const mainnetClient = createPublicClient({
   chain: mainnet,
-  transport: http(
-    `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-  ),
+  transport: http(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
 });
 
 const optimismClient = createPublicClient({
   chain: optimism,
-  transport: http(
-    `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-  ),
+  transport: http(`https://optimism-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
 });
 
 const baseClient = createPublicClient({
   chain: base,
-  transport: http(process.env.BASE_RPC_URL!),
+  transport: fallback([
+    http(process.env.BASE_RPC_URL!),
+    http(process.env.BASE_RPC_URL2!),
+    http(process.env.BASE_RPC_URL3!),
+    http(process.env.BASE_RPC_URL4!),
+  ]),
 });
 
 const arbitrumClient = createPublicClient({
   chain: arbitrum,
-  transport: http(
-    `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-  ),
+  transport: http(`https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
 });
 
 const zoraClient = createPublicClient({
   chain: zora,
-  transport: http(
-    `https://rpc.zora.energy`
-  ),
+  transport: http(`https://rpc.zora.energy`),
 });
-
-
 
 export const clientsByChainId = {
   [String(mainnet.id)]: mainnetClient,
   [String(optimism.id)]: optimismClient,
   [String(base.id)]: baseClient,
   [String(arbitrum.id)]: arbitrumClient,
-  [String(zora.id)]: zoraClient
+  [String(zora.id)]: zoraClient,
 };
 
 export const chainByChainId = {
@@ -49,5 +44,5 @@ export const chainByChainId = {
   [String(optimism.id)]: optimism,
   [String(base.id)]: base,
   [String(arbitrum.id)]: arbitrum,
-  [String(zora.id)]: zora
+  [String(zora.id)]: zora,
 };
