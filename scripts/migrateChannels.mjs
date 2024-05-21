22fcc0e7dded97a46079fdbf6587b6cdd894519e
jtgi <j@jtgi.me>
2024-05-21 21:52:52 +0900

    checkpoint

---
+++ scripts/migrateChannels.mjs
@@ -0,0 +1,165 @@
+import fs from "node:fs/promises";
+import sqlite3 from "sqlite3";
+
+async function main() {
+  await fs.copyFile("./sqlite.db", "./sqlite.db.bak");
+  const db = new sqlite3.Database("./sqlite.db.bak");
+
+  db.all("SELECT * FROM ruleset", [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    rows.forEach((row) => {
+      let migratedRow = migrate(row);
+      const updatedJson = JSON.stringify(migratedRow);
+
+      // Update each row
+      db.run("UPDATE ruleset SET rule = ? WHERE rowid = ?", [updatedJson, row.rowid], (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
+
+  // Close the database connection
+  db.close();
+}
+
+function migrate(row) {
+  let rules = JSON.parse(row.rule);
+  rules.conditions.forEach((condition) => {
+    switch (condition.type) {
+      case "textMatchesPattern": {
+        break;
+      }
+      case "textMatchesLanguage": {
+        break;
+      }
+      case "containsText": {
+        break;
+      }
+      case "containsTooManyMentions": {
+        break;
+      }
+      case "containsLinks": {
+        break;
+      }
+      case "containsEmbeds": {
+        break;
+      }
+      case "castInThread": {
+        break;
+      }
+      case "castLength": {
+        break;
+      }
+      case "downvote": {
+        break;
+      }
+      case "userProfileContainsText": {
+        break;
+      }
+      case "userDoesNotFollow": {
+        break;
+      }
+      case "userIsCohost": {
+        break;
+      }
+      case "userDisplayNameContainsText": {
+        break;
+      }
+      case "userFollowerCount": {
+        break;
+      }
+      case "userIsNotActive": {
+        break;
+      }
+      case "userDoesNotHoldPowerBadge": {
+        break;
+      }
+      case "userFidInRange": {
+        break;
+      }
+      case "requireActiveHypersub": {
+        break;
+      }
+      case "requiresErc721": {
+        break;
+      }
+      case "requiresErc20": {
+        break;
+      }
+      case "requiresErc1155": {
+        break;
+      }
+    }
+  });
+
+  let actions = JSON.parse(row.actions || "[]");
+  actions.forEach((action) => {
+    switch (action.type) {
+      case "bypass": {
+        break;
+      }
+      case "addToBypass": {
+        break;
+      }
+      case "hideQuietly": {
+        action.type = "like";
+        break;
+      }
+      case "downvote": {
+        break;
+      }
+      case "ban": {
+        break;
+      }
+      case "like": {
+        break;
+      }
+      case "mute": {
+        break;
+      }
+      case "warnAndHide": {
+        break;
+      }
+      case "cooldown": {
+        break;
+      }
+      case "cooldownEnded": {
+        break;
+      }
+      case "unhide": {
+        break;
+      }
+      case "unmuted": {
+        break;
+      }
+      case "grantRole": {
+        break;
+      }
+    }
+  });
+}
+
+/**
+ * 
+ * action based logic
+  14 ban
+   5 cooldown
+   6 cooldown,hideQuietly
+   3 cooldown,warnAndHide
+ 101 hideQuietly
+  36 like
+   4 mute
+  43 warnAndHide
+
+  if action contains cooldown
+    leave it
+
+  if like do nothing
+ */
+
+main();
