1277d0830345e1ab15e729cd2c2f97e50160c839
jtgi <j@jtgi.me>
2024-06-22 22:06:34 +0900

    hc'

---
+++ app/routes/api.healthcheck.tsx
@@ -0,0 +1,28 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import axios from "axios";
+import { db } from "~/lib/db.server";
+import { sendNotification } from "~/lib/notifications.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  //using as a cheap cron
+  checkEvents().catch(console.error);
+
+  return json({ status: "ok" });
+}
+
+async function checkEvents() {
+  const castsProcessedLastMinute = await db.castLog.findMany({
+    where: {
+      createdAt: {
+        gte: new Date(Date.now() - 60000),
+      },
+    },
+  });
+
+  if (castsProcessedLastMinute.length < 5) {
+    console.log("Warning: Less than 5 casts processed in the last minute");
+    await axios.post("https://webhook-relay.fly.dev/automod", {
+      text: "Warning: Less than 5 casts processed in the last minute",
+    });
+  }
+}
