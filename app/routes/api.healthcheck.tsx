9a5e4149aee08b8feb031d18428ec2f2d0cea173
jtgi <j@jtgi.me>
2024-06-22 22:25:02 +0900

    tyr this

--- app/routes/api.healthcheck.tsx
+++ app/routes/api.healthcheck.tsx
@@ -14,13 +14,13 @@ async function checkEvents() {
   const castsProcessedLastMinute = await db.castLog.findMany({
     where: {
       createdAt: {
-        gte: new Date(Date.now() - 60000),
+        gte: new Date(Date.now() - 5 * 6_000),
       },
     },
   });
 
   if (castsProcessedLastMinute.length < 5) {
-    console.log("Warning: Less than 5 casts processed in the last minute");
+    console.log("Warning: Less than 5 casts processed in the last 5 minutes");
     await axios.post("https://webhook-relay.fly.dev/automod", {
       text: "Warning: Less than 5 casts processed in the last minute",
     });
