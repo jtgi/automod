9dab4cfa9528d04347c5a6f8c37bb17dd4a82f5e
jtgi <j@jtgi.me>
2024-09-04 12:58:10 +0900

    sync status

--- app/routes/status.tsx
+++ app/routes/status.tsx
@@ -1,11 +1,12 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
+import { Link } from "lucide-react";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
 import { db } from "~/lib/db.server";
 
 export async function loader() {
   const checks = await db.propagationDelayCheck.findMany({
-    where: {
-      arrivedAt: null,
+    take: 5,
+    orderBy: {
+      createdAt: "desc",
     },
   });
 
@@ -16,9 +17,28 @@ export default function Status() {
   const { checks } = useTypedLoaderData<typeof loader>();
 
   return (
-    <div>
+    <div className="flex flex-col gap-4 p-12">
       <h1>Sync Status</h1>
-      <pre>{JSON.stringify(checks, null, 2)}</pre>
+      {checks.map((check) => (
+        <table key={check.id}>
+          <thead>
+            <tr>
+              <th>Created At</th>
+              <th>Arrived At</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{check.createdAt.toLocaleString()}</td>
+              <td>{check.arrivedAt?.toLocaleString() || "-"}</td>
+              <td>
+                <Link to={`https://explorer.neynyar.com/${check.hash}`}>Explorer</Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
     </div>
   );
 }
