4a65a9f6b1c2d94b2011e48129b3d503f191034d
jtgi <j@jtgi.me>
2024-09-20 13:15:34 +0900

    partner id

--- app/routes/api.partners.channels.$id.tsx
+++ app/routes/api.partners.channels.$id.tsx
@@ -2,6 +2,7 @@ import { json, LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { requirePartnerApiKey } from "~/lib/utils.server";
 import { db } from "~/lib/db.server";
+import { Rule, ruleDefinitions } from "~/lib/validations.server";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   await requirePartnerApiKey({ request });
@@ -45,7 +46,28 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     slowModeHours,
     excludeCohosts,
     excludeUsers: excludeUsernamesParsed,
+    membershipRequirements: filterUserRules(inclusionRuleSetParsed?.ruleParsed),
     inclusionRuleSet: inclusionRuleSetParsed?.ruleParsed,
     exclusionRuleSet: exclusionRuleSetParsed?.ruleParsed,
   });
+}
+
+function filterUserRules(rule: Rule | undefined) {
+  if (!rule || !rule.conditions) {
+    return rule;
+  }
+
+  const userScopedRules: Rule[] = [];
+
+  for (const cond of rule.conditions) {
+    const ruleDef = ruleDefinitions[cond.name];
+    if (ruleDef.checkType === "user") {
+      userScopedRules.push(cond);
+    }
+  }
+
+  return {
+    ...rule,
+    conditions: userScopedRules,
+  };
 }
